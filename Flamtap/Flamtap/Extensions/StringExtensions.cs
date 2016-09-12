bfb8792465c0e9998702bb0647236a52c4f52a52
Eric Sondergard <flamtap@users.noreply.github.com>
2016-09-11 20:05:47 -0600

    TableParser and string extensions
    TableParser class for printing collections in a tabular format, string
    extension for splitting a string by "camel-case" (eg.: "GitHub" => "Git
    Hub" or "SQLServer2016" => "SQL Server 2016")

---
+++ Flamtap/Flamtap/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Flamtap.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string ToDisplayText(this string str)
+        {
+            return string.Join(" ", Regex.Split(str, @"(\B[A-Z]+?(?=[A-Z][^A-Z])|\B[A-Z]+?(?=[^A-Z]))"));
+        }
+    }
+}
