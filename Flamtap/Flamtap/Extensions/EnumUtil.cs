9e7e0a999b1870cb57874f24d6538bbdf78f2dae
Eric Sondergard <flamtap@users.noreply.github.com>
2016-10-14 13:26:02 -0600

    EnumUtil class
    Enum util static class with method for parsing a string and returning it's associated enum value.

---
+++ Flamtap/Flamtap/Extensions/EnumUtil.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Flamtap.Extensions
+{
+    public static class EnumUtil
+    {
+        /// <summary>
+        /// Parses a string value to it's equivalent enum value.
+        /// </summary>
+        /// <typeparam name="T">The enum type.</typeparam>
+        /// <returns> The enum value with specified name.</returns>
+    	  public static T Parse<T>(string value)
+    	  {
+    		    return (T) Enum.Parse(typeof (T), value, true);
+    	  }
+    }
+}
