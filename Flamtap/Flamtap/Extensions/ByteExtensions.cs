26fe0efbc30dfa943e9d07f9bb7abf3d1f66aa15
Eric Sondergard <flamtap@users.noreply.github.com>
2017-05-09 11:12:24 -0600

    Add method to read bits from middle of byte

--- Flamtap/Flamtap/Extensions/ByteExtensions.cs
+++ Flamtap/Flamtap/Extensions/ByteExtensions.cs
@@ -20,5 +20,11 @@ namespace mcThings.Extensions
             byte mask = (byte) ((1 << n) - 1);
             return (byte) (value & mask);
         }
+        
+        public static byte ReadBitsAt(this byte value, byte startIndex, byte count)
+        {
+            byte first = value.ReadFirstNBits((byte) (count + startIndex));
+            return first.ReadLastNBits(count);
+        }
     }
 }
