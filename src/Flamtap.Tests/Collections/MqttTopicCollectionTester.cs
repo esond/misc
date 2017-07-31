7474d428b7300e416618a50f8f1cdca25b1a4a52
Eric Sondergard <flamtap@users.noreply.github.com>
2017-07-30 22:10:57 -0600

    WIP

---
+++ src/Flamtap.Tests/Collections/MqttTopicCollectionTester.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Flamtap.Collections;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Flamtap.Tests.Collections
+{
+    [TestFixture]
+    public class MqttTopicCollectionTester
+    {
+        #region Test Data
+
+        public static IEnumerable<string> ValidTopics => TestConstants.ValidMqttTopics;
+
+        public static IEnumerable<string> InvalidTopics => TestConstants.InvalidMqttTopics;
+
+        #endregion
+
+        [SetUp]
+        public void SetUp()
+        {
+            _collection = new MqttTopicCollection<string>();
+        }
+
+        private MqttTopicCollection<string> _collection;
+
+        [Test]
+        [TestCaseSource(nameof(ValidTopics))]
+        public void add_should_not_throw_exception_for_valid_topics(string validTopic)
+        {
+            Action add = () => _collection.Add(validTopic, "foo");
+            add.ShouldNotThrow<Exception>();
+        }
+
+        //[Test]
+        //[TestCaseSource(nameof(ValidTopics))]
+        //public void add_by_index_should_not_throw_exception_for_valid_topics(string validTopic)
+        //{
+        //    Action add = () => _collection[validTopic] = "foo";
+        //    add.ShouldNotThrow<Exception>();
+        //}
+
+        //[Test]
+        //[TestCaseSource(nameof(ValidTopics))]
+        //public void add_by_keyValuePair_should_not_throw_exception_for_valid_topics(string validTopic)
+        //{
+        //    Action add = () => _collection.Add(new KeyValuePair<string, string>(validTopic, "foo"));
+        //    add.ShouldNotThrow<Exception>();
+        //}
+
+        [Test]
+        [TestCaseSource(nameof(InvalidTopics))]
+        public void add_should_throw_exception_for_invalid_topics(string invalidTopic)
+        {
+            Action add = () => _collection.Add(invalidTopic, "foo");
+            add.ShouldThrow<ArgumentException>();
+        }
+
+        //[Test]
+        //[TestCaseSource(nameof(InvalidTopics))]
+        //public void add_by_index_should_throw_exception_for_invalid_topics(string invalidTopic)
+        //{
+        //    Action add = () => _collection[invalidTopic] = "foo";
+        //    add.ShouldThrow<ArgumentException>();
+        //}
+
+        //[Test]
+        //[TestCaseSource(nameof(InvalidTopics))]
+        //public void add_by_keyValuePair_should_throw_exception_for_invalid_topics(string invalidTopic)
+        //{
+        //    Action add = () => _collection.Add(new KeyValuePair<string, string>(invalidTopic, "foo"));
+        //    add.ShouldThrow<ArgumentException>();
+        //}
+
+        [Test]
+        public void getMatches_should_match_multi_level_wildcards()
+        {
+            const string match = "match";
+            const string noMatch = "nomatch";
+
+            const string queryTopic = "foo/bar/#";
+
+            _collection.Add("#", match);
+            _collection.Add("foo/#", match);
+            _collection.Add("foo/bar", match);
+            _collection.Add("foo/bar/1/#", match);
+            _collection.Add("foo/bar/1/2/3", match);
+            _collection.Add("foo/+/", match);
+
+            _collection.Add("sut/baz/1", noMatch);
+            _collection.Add("foo", noMatch);
+
+            IEnumerable<KeyValuePair<string, string>> matches = _collection.GetMatches(queryTopic).ToList();
+            
+            matches.Select(m => m.Value).ShouldAllBeEquivalentTo(match);
+        }
+
+        [Test]
+        public void getMatches_should_match_single_level_wildcards()
+        {
+            const string match = "match";
+            const string noMatch = "nomatch";
+
+            const string queryTopic = "foo/bar/+";
+
+            _collection.Add("#", match);
+            _collection.Add("foo/#", match);
+            _collection.Add("foo/bar/1", match);
+
+            _collection.Add("foo/bar", noMatch);
+            _collection.Add("foo/bar/1/#", noMatch);
+            _collection.Add("foo/bar/1/2/3", noMatch);
+            _collection.Add("foo/+/", noMatch);
+            _collection.Add("sut/baz/1", noMatch);
+            _collection.Add("foo", noMatch);
+
+            IEnumerable<KeyValuePair<string, string>> matches = _collection.GetMatches(queryTopic).ToList();
+
+            matches.Select(m => m.Value).ShouldAllBeEquivalentTo(match);
+        }
+
+        [Test]
+        public void getMatches_should_match_single_and_multi_level_wildcards()
+        {
+            const string match = "match";
+            const string noMatch = "nomatch";
+
+            const string queryTopic = "foo/bar/+/#";
+
+            _collection.Add("#", match);
+            _collection.Add("foo/#", match);
+            _collection.Add("foo/bar/1", match);
+            _collection.Add("foo/bar/#", match);
+            _collection.Add("foo/bar/1/#", match);
+            _collection.Add("foo/bar/1/2/3", match);
+
+            _collection.Add("foo/+/", noMatch);
+            _collection.Add("foo", noMatch);
+            _collection.Add("foo/bar", noMatch);
+            _collection.Add("sut/baz/1", noMatch);
+
+            IEnumerable<KeyValuePair<string, string>> matches = _collection.GetMatches(queryTopic).ToList();
+
+            matches.Select(m => m.Value).ShouldAllBeEquivalentTo(match);
+        }
+    }
+}
+
---
+++ src/Flamtap/Collections/MqttTopicCollection.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Flamtap.Extensions;
+
+namespace Flamtap.Collections
+{
+    public class MqttTopicDictionary<TValue> : IDictionary<string, TValue>
+    {
+        private const string Separator = "/";
+        private const string MultiLevel = "#";
+        private const string SingleLevel = "+";
+
+        private readonly IDictionary<string, TValue> _dict;
+
+        public MqttTopicDictionary() => _dict = new Dictionary<string, TValue>();
+
+        public MqttTopicDictionary(IDictionary<string, TValue> dictionary) => _dict =
+            new Dictionary<string, TValue>(dictionary);
+
+        public MqttTopicDictionary(int capacity) => _dict = new Dictionary<string, TValue>(capacity);
+
+        public IEnumerable<KeyValuePair<string, TValue>> GetMatches(string topic)
+        {
+            Regex regex = MakeRegex(topic);
+
+            IEnumerable<KeyValuePair<string, TValue>> keyValuePairs = _dict.Where(d => regex.IsMatch(d.Key));
+
+            return _dict.Where(d => regex.IsMatch(d.Key));
+        }
+
+        private static Regex MakeRegex(string topic)
+        {
+            IEnumerable<string> tokens = topic.Split(Separator.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> parts = new List<string>();
+
+            foreach (string token in tokens)
+            {
+                bool isLast = token == tokens.Last();
+
+                if (token == SingleLevel)
+                {
+                    parts.Add(isLast ? @"([^/#+]+/)" : @".*/");
+
+                    continue;
+                }
+
+                if (token == MultiLevel)
+                {
+                    parts.Add(@"((?:[^/]/?)*)");
+                    break;
+                }
+
+                if (isLast)
+                    parts.Add(token);
+                else
+                    parts.Add($"{token}/");
+            }
+
+            string pattern = $"^{string.Join(string.Empty, parts)}$";
+
+            return new Regex(pattern);
+        }
+
+        #region Implementation of IDictionary<string,TValue>
+
+        public bool ContainsKey(string key)
+        {
+            return _dict.ContainsKey(key);
+        }
+
+        public void Add(string key, TValue value)
+        {
+            if (!key.IsValidMqttTopic())
+                throw new ArgumentException("Invalid MQTT topic.", nameof(key));
+
+            _dict.Add(key, value);
+        }
+
+        public bool Remove(string key)
+        {
+            return _dict.Remove(key);
+        }
+
+        public bool TryGetValue(string key, out TValue value)
+        {
+            return _dict.TryGetValue(key, out value);
+        }
+
+        public TValue this[string key]
+        {
+            get => _dict[key];
+            set
+            {
+                if (!key.IsValidMqttTopic())
+                    throw new ArgumentException("Invalid MQTT topic.", nameof(key));
+
+                _dict[key] = value;
+            }
+        }
+
+        public ICollection<string> Keys => _dict.Keys;
+
+        public ICollection<TValue> Values => _dict.Values;
+
+        #endregion
+
+        #region Implementation of ICollection<KeyValuePair<string,TValue>>
+
+        public void Add(KeyValuePair<string, TValue> item)
+        {
+            if (!item.Key.IsValidMqttTopic())
+                throw new ArgumentException("Invalid MQTT topic.", nameof(item));
+
+            _dict.Add(item);
+        }
+
+        public void Clear()
+        {
+            _dict.Clear();
+        }
+
+        public bool Contains(KeyValuePair<string, TValue> item)
+        {
+            return _dict.Contains(item);
+        }
+
+        public void CopyTo(KeyValuePair<string, TValue>[] array, int arrayIndex)
+        {
+            _dict.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(KeyValuePair<string, TValue> item)
+        {
+            return _dict.Remove(item);
+        }
+
+        public int Count => _dict.Count;
+
+        public bool IsReadOnly => _dict.IsReadOnly;
+
+        #endregion
+
+        #region Implementation of IEnumerable
+
+        public IEnumerator<KeyValuePair<string, TValue>> GetEnumerator()
+        {
+            return _dict.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)_dict).GetEnumerator();
+        }
+
+        #endregion
+    }
+}
+
