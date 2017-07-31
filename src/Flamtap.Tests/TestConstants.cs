e4a4ff420aff77d575ed76a3f35662d929bb0319
Eric Sondergard <flamtap@users.noreply.github.com>
2017-07-30 18:16:09 -0600

    Add MqttTopicValidator and tests.

---
+++ src/Flamtap.Tests/TestConstants.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace Flamtap.Tests
+{
+    public class TestConstants
+    {
+        #region Mqtt Topics
+
+        public static IEnumerable<string> ValidMqttTopics
+        {
+            get
+            {
+                yield return "+";
+                yield return "+/+";
+                yield return "#";
+                yield return "A";
+                yield return "A/";
+                yield return "A/+";
+                yield return "A/#";
+                yield return "A/B";
+                yield return "A/B/C";
+                yield return "A/+/C";
+                yield return "A/+/C/#";
+                yield return "A//+//C/#";
+                yield return "/A/+//////C/#/";
+                yield return "/A/$%^&/#/";
+            }
+        }
+
+        public static IEnumerable<string> InvalidMqttTopics
+        {
+            get
+            {
+                yield return string.Empty;
+                yield return "/";
+                yield return "A/#/C";
+                yield return "A/##/C";
+                yield return "A/B/##";
+                yield return "A/foo#";
+                yield return "A/+/++/D";
+                yield return "foo/bar+/D/";
+                yield return "A/кир/+/D";
+                yield return "A/한자/D";
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/Flamtap/Validation/Utf8Validator.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Flamtap.Validation
+{
+    public static class Utf8Validator
+    {
+        /// <summary>
+        ///     Check if stream is UTF-8 encoded.
+        /// </summary>
+        /// <param name="stream">Stream to read from.</param>
+        /// <returns>True if the whole stream is UTF-8 encoded.</returns>
+        public static bool IsUtf8(Stream stream)
+        {
+            int count = 4 * 1024;
+            byte[] buffer;
+            int read;
+
+            while (true)
+            {
+                buffer = new byte[count];
+                stream.Seek(0, SeekOrigin.Begin);
+                read = stream.Read(buffer, 0, count);
+
+                if (read < count)
+                    break;
+
+                count *= 2;
+            }
+            return IsUtf8(buffer, read);
+        }
+        
+        private static bool IsUtf8(IReadOnlyList<byte> buffer, int length)
+        {
+            int position = 0;
+            int bytes = 0;
+            while (position < length)
+            {
+                if (!IsValid(buffer, position, length, ref bytes))
+                    return false;
+                position += bytes;
+            }
+            return true;
+        }
+
+        private static bool IsValid(IReadOnlyList<byte> buffer, int position, int length, ref int bytes)
+        {
+            if (length > buffer.Count)
+                throw new ArgumentException("Invalid length.", nameof(length), null);
+
+            if (position > length - 1)
+            {
+                bytes = 0;
+                return true;
+            }
+
+            byte ch = buffer[position];
+
+            if (ch <= 0x7F)
+            {
+                bytes = 1;
+                return true;
+            }
+
+            if (ch >= 0xc2 && ch <= 0xdf)
+            {
+                if (position >= length - 2)
+                {
+                    bytes = 0;
+                    return false;
+                }
+                if (buffer[position + 1] < 0x80 || buffer[position + 1] > 0xbf)
+                {
+                    bytes = 0;
+                    return false;
+                }
+                bytes = 2;
+                return true;
+            }
+
+            if (ch == 0xe0)
+            {
+                if (position >= length - 3)
+                {
+                    bytes = 0;
+                    return false;
+                }
+
+                if (buffer[position + 1] < 0xa0 || buffer[position + 1] > 0xbf ||
+                    buffer[position + 2] < 0x80 || buffer[position + 2] > 0xbf)
+                {
+                    bytes = 0;
+                    return false;
+                }
+                bytes = 3;
+                return true;
+            }
+
+
+            if (ch >= 0xe1 && ch <= 0xef)
+            {
+                if (position >= length - 3)
+                {
+                    bytes = 0;
+                    return false;
+                }
+
+                if (buffer[position + 1] < 0x80 || buffer[position + 1] > 0xbf ||
+                    buffer[position + 2] < 0x80 || buffer[position + 2] > 0xbf)
+                {
+                    bytes = 0;
+                    return false;
+                }
+
+                bytes = 3;
+                return true;
+            }
+
+            if (ch == 0xf0)
+            {
+                if (position >= length - 4)
+                {
+                    bytes = 0;
+                    return false;
+                }
+
+                if (buffer[position + 1] < 0x90 || buffer[position + 1] > 0xbf ||
+                    buffer[position + 2] < 0x80 || buffer[position + 2] > 0xbf ||
+                    buffer[position + 3] < 0x80 || buffer[position + 3] > 0xbf)
+                {
+                    bytes = 0;
+                    return false;
+                }
+
+                bytes = 4;
+                return true;
+            }
+
+            if (ch == 0xf4)
+            {
+                if (position >= length - 4)
+                {
+                    bytes = 0;
+                    return false;
+                }
+
+                if (buffer[position + 1] < 0x80 || buffer[position + 1] > 0x8f ||
+                    buffer[position + 2] < 0x80 || buffer[position + 2] > 0xbf ||
+                    buffer[position + 3] < 0x80 || buffer[position + 3] > 0xbf)
+                {
+                    bytes = 0;
+                    return false;
+                }
+
+                bytes = 4;
+                return true;
+            }
+
+            if (ch >= 0xf1 && ch <= 0xf3)
+            {
+                if (position >= length - 4)
+                {
+                    bytes = 0;
+                    return false;
+                }
+
+                if (buffer[position + 1] < 0x80 || buffer[position + 1] > 0xbf ||
+                    buffer[position + 2] < 0x80 || buffer[position + 2] > 0xbf ||
+                    buffer[position + 3] < 0x80 || buffer[position + 3] > 0xbf)
+                {
+                    bytes = 0;
+                    return false;
+                }
+
+                bytes = 4;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
