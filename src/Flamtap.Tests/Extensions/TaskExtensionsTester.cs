aba9be5410faf9cbc5ca1df4aa26113d106ae9c8
Eric Sondergard <flamtap@users.noreply.github.com>
2017-10-22 18:45:13 -0600

    Add TaskExtensions class with extension methods to invoke a delegate if a task does or doesn't throw an exception.

---
+++ src/Flamtap.Tests/Extensions/TaskExtensionsTester.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Threading.Tasks;
+using Flamtap.Extensions;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Flamtap.Tests.Extensions
+{
+    [TestFixture]
+    public class TaskExtensionsTester
+    {
+        [Test]
+        public async Task Success_delegate_should_execute_if_task_does_not_throw_exception()
+        {
+            var result = false;
+
+            await Task.CompletedTask.TryAsync<Exception>(() => result = true, e => result = false);
+
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public async Task Success_delegate_should_execute_if_typed_task_does_not_throw_exception()
+        {
+            var result = false;
+
+            int value = await Task.Run(() => -1).TryAsync<int, Exception>(
+                onSuccess: r =>
+                {
+                    result = true;
+                },
+                onError: e =>
+                {
+                    result = false;
+                });
+
+            value.ShouldBeEquivalentTo(-1);
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public async Task Error_delegate_should_execute_if_task_throws_exception()
+        {
+            var result = true;
+
+            var task = Task.Run(() => throw new Exception());
+
+            await task.TryAsync<Exception>(() => result = true, e => result = false);
+
+            result.Should().BeFalse();
+        }
+
+        [Test]
+        public async Task Error_delegate_should_execute_if_typed_task_throws_exception()
+        {
+            var result = true;
+
+            int value = await Task.Run(() =>
+            {
+                var i = 1;
+
+                if (i == 1)
+                    throw new Exception();
+
+                return i;
+            }).TryAsync<int, Exception>(i => result = true, e => result = false);
+
+            value.ShouldBeEquivalentTo(0);
+            result.Should().BeFalse();
+        }
+
+        [Test]
+        public async Task Error_delegate_should_execute_if_thrown_exception_is_subtype_of_exception_type1()
+        {
+            var result = true;
+
+            var task = Task.Run(() => throw new InvalidOperationException());
+
+            await task.TryAsync<Exception>(() => result = true, e => result = false);
+
+            result.Should().BeFalse();
+        }
+
+        [Test]
+        public async Task Error_delegate_should_execute_if_thrown_exception_is_subtype_of_exception_type2()
+        {
+            var result = true;
+
+            int value = await Task.Run(() =>
+            {
+                var i = 1;
+
+                if (i == 1)
+                    throw new InvalidOperationException();
+
+                return i;
+            }).TryAsync<int, Exception>(i => result = true, e => result = false);
+
+            value.ShouldBeEquivalentTo(0);
+            result.Should().BeFalse();
+        }
+
+        [Test]
+        public void Exception_should_be_thrown_if_execution_throws_a_different_exception_than_specified1()
+        {
+            Func<Task> func = async () =>
+            {
+                var task = Task.Run(() => throw new DummyException());
+
+                await task.TryAsync<InvalidOperationException>(() => { }, e => { });
+            };
+
+            func.ShouldThrowExactly<DummyException>();
+        }
+
+        [Test]
+        public void Exception_should_be_thrown_if_execution_throws_a_different_exception_than_specified2()
+        {
+            Func<Task> func = async () =>
+            {
+                await Task.Run(() =>
+                {
+                    var i = 1;
+
+                    if (i == 1)
+                        throw new DummyException();
+
+                    return i;
+                }).TryAsync<int, InvalidOperationException>(i => { }, e => { });
+            };
+
+            func.ShouldThrowExactly<DummyException>();
+        }
+
+        private class DummyException : Exception
+        {
+        }
+    }
+}
---
+++ src/Flamtap/Extensions/TaskExtensions.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Flamtap.Extensions
+{
+    public static class TaskExtensions
+    {
+        /// <summary>
+        ///     Awaits a task, invoking one of two actions based on if the execution of the task throws
+        ///     an exception of the specified type.
+        /// </summary>
+        /// <typeparam name="TException">
+        ///     The type of exception that, if thrown, will cause the <paramref name="onError"/> delegate to be
+        ///     executed.
+        /// </typeparam>
+        /// <param name="task">The task to execute.</param>
+        /// <param name="onSuccess">
+        ///     The delegate that represents the code to execute if the task completes without throwing and exception
+        ///     of type <typeparamref name="TException"/>.
+        /// </param>
+        /// <param name="onError">
+        ///     The delegate that represents the code to execute if and eception of type
+        ///     <typeparamref name="TException"/> is thrown during execution.
+        /// </param>
+        /// <returns>The result of the awaited task.</returns>
+        public static async Task TryAsync<TException>(this Task task, Action onSuccess, Action<TException> onError)
+            where TException : Exception
+        {
+            try
+            {
+                await task;
+            }
+            catch (TException e)
+            {
+                onError.Invoke(e);
+                return;
+            }
+
+            onSuccess.Invoke();
+        }
+
+        /// <summary>
+        ///     Awaits a task, invoking one of two functions based on whether or not the execution of the task throws
+        ///     an exception of the specified type.
+        /// </summary>
+        /// <typeparam name="TResult">The result type of the task.</typeparam>
+        /// <typeparam name="TException">
+        ///     The type of exception that, if thrown, will cause the <paramref name="onError"/> delegate to be
+        ///     executed.
+        /// </typeparam>
+        /// <param name="task">The task to execute.</param>
+        /// <param name="onSuccess">
+        ///     The delegate that represents the code to execute if the task completes without throwing and exception
+        ///     of type <typeparamref name="TException"/>.
+        /// </param>
+        /// <param name="onError">
+        ///     The delegate that represents the code to execute if and eception of type
+        ///     <typeparamref name="TException"/> is thrown during execution.
+        /// </param>
+        /// <returns>The result of the awaited task.</returns>
+        public static async Task<TResult> TryAsync<TResult, TException>(this Task<TResult> task,
+            Action<TResult> onSuccess, Action<TException> onError)
+            where TException : Exception
+        {
+            TResult result;
+
+            try
+            {
+                result = await task;
+            }
+            catch (TException e)
+            {
+                onError.Invoke(e);
+                return default(TResult);
+            }
+            
+            onSuccess.Invoke(result);
+
+            return result;
+        }
+    }
+}
